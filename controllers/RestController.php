70092844d302b3fa02a7a693c00389f2c2175525
unknown <mygarbage86@mail.ru>
2021-09-18 10:23:51 +0300

    fixed rest controller cors

--- controllers/RestController.php
+++ controllers/RestController.php
@@ -23,7 +23,12 @@ abstract class RestController extends ActiveController
         $behaviors['corsFilter'] = [
             'class' => \yii\filters\Cors::class,
             'cors' => [
-                'Origin' => ['http://localhost:4200', 'http://localhost:4201'],
+                'Origin' => [
+                    'http://localhost:4200',
+                    'http://localhost:4201',
+                    'http://ortum.injini.ru',
+                    'http://cab.ortum.injini.ru'
+                ],
                 'Access-Control-Allow-Origin' => true,
                 'Access-Control-Allow-Credentials' => true,
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
